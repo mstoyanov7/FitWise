b3b4b1abc005d34c900012b4d7422e141be06364
mstoyanov16 <marin.stoyanov123@gmail.com>
2025-04-02 00:52:50 +0300

    Initial implementation of barcode scanner

---
+++ app/src/main/java/com/example/workoutapp/MyCaptureActivity.java
@@ -0,0 +1,7 @@
+package com.example.workoutapp;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class MyCaptureActivity extends CaptureActivity {
+    // Просто празно разширение, за override на манифест конфигурация
+}
