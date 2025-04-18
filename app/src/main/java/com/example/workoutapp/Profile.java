900e319f161025a4a634de5dd5d5d784a9b499bc
mstoyanov7 <92221668+mstoyanov7@users.noreply.github.com>
2025-04-18 15:47:13 +0300

    Mariyan fixed conflict here!

--- app/src/main/java/com/example/workoutapp/Profile.java
+++ app/src/main/java/com/example/workoutapp/Profile.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +14,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -121,7 +123,8 @@ public class Profile extends AppCompatActivity {
                     return true;
                 }
                 return false;
-          });
+            }
+        });
 
         // Select initial tab
         String selectedTab = getIntent().getStringExtra("selectedTab");
